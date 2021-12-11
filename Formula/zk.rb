4c0ebb96a8d91a791cba639009c48d35f5d1a509
Mitchell Hanberg <mitch@mitchellhanberg.com>
2021-12-11 16:10:37 -0500

    Use different syntax for head block

--- Formula/zk.rb
+++ Formula/zk.rb
@@ -5,16 +5,18 @@ class Zk < Formula
   sha256 "b1967e93469aa6abdc49d0cbd21f603576bcf09d94c2a13a6dce78bbf2983805"
   license "GPL-3.0-only"
 
-  head "https://github.com/mickael-menu/zk.git", branch: "main"
-
   bottle do
     root_url "https://github.com/mhanberg/homebrew-zk/releases/download/zk-0.8.0"
     sha256 cellar: :any,                 catalina:     "5aba83544f414b29498c4fc565d0b14e217d5c2465a052f724eddb60daf1b574"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "f6a55d183206f4d60b20825a7e1ff9d3f4b1c4f07dd2b8092b98572ff245d55b"
   end
 
+  head do
+    url "https://github.com/mickael-menu/zk.git", branch: "main"
+    depends_on "git-lfs" => :build
+  end
+
   depends_on "go" => :build
-  depends_on "git-lfs" => :build
 
   depends_on "icu4c"
   depends_on "sqlite3"
